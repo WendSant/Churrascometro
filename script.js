0ab84561d89b2deece5099ac0529ee8be6b7999f
Wendson <wendsonsantanajps@gmail.com>
2021-09-03 22:17:02 -0300

    Bug solved

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Regras do aplicativo
 //  Carne - 400gr por pessoa + de 6horas - 650
 // Cerveja - 1200ml por Pessoa + 6 horas - 2000ml
@@ -9,33 +8,29 @@
 let inputAdultos = document.getElementById("adultos");
 let inputCriancas = document.getElementById("criancas");
 let inputDuracao = document.getElementById("duracao");
-let resultado = document.getElementById("resultado")
-
+let resultado = document.getElementById("resultado");
 
 // Funções
 function carne(duracao) {
-  
-  if(duracao >= 6) {
+  if (duracao >= 6) {
     return 650;
-  }else{
+  } else {
     return 400;
   }
 }
 
 function cerveja(duracao) {
-  
-  if(duracao >= 6) {
+  if (duracao >= 6) {
     return 2000;
-  }else{
+  } else {
     return 1200;
   }
 }
 
 function bebidas(duracao) {
-  
-  if(duracao >= 6) {
+  if (duracao >= 6) {
     return 1500;
-  }else{
+  } else {
     return 1000;
   }
 }
@@ -45,64 +40,50 @@ function calcular() {
   let criancas = inputCriancas.value;
   let duracao = inputDuracao.value;
 
-  if (adultos <=0) {
-    alert('Digite quantos adultos');
-  }
-  else if (criancas <=0) {
-    alert('Digite quantas crianças');
-  }
-  else if (duracao <=0) {
-    alert('Digite a duração do churrasco');
-  }
-  else {
-
-    let qdtTotalCarne = carne(duracao) * adultos + (carne(duracao) / 2 * criancas);
+  if (adultos <= 0) {
+    alert("Digite quantos adultos");
+  } else if (criancas <= 0) {
+    alert("Digite quantas crianças");
+  } else if (duracao <= 0) {
+    alert("Digite a duração do churrasco");
+  } else {
+    let qdtTotalCarne =
+      carne(duracao) * adultos + (carne(duracao) / 2) * criancas;
     let qdtTotalCerveja = cerveja(duracao) * adultos;
-    let qdtTotalBebidas = bebidas(duracao) * adultos + (bebidas(duracao) / 2 * criancas);
+    let qdtTotalBebidas =
+      bebidas(duracao) * adultos + (bebidas(duracao) / 2) * criancas;
 
-  
-
-  
-  // resultado.innerHTML = `<h2>Você vai precisar de:</h2>`
-  // resultado.innerHTML += `
-  //   <div class="result-block">
-  //     <img src="./assets/steak2.svg"/>
-  //     <p>${qdtTotalCarne/1000} Kg de carne</p>
-  //   </div>
-  // `
-
-  //Se a quantidade de carne for menor que 1k sera mostrado em gramas não em kg e acima de 999 gramas vai ser mostrado em kg
-
-  if (qdtTotalCarne < 1000){
-    resultado.innerHTML += `
+    if (qdtTotalCarne < 1000) {
+      resultado.innerHTML = `
     <div class="result-block">
       <img src="./assets/steak2.svg"/>
       <p>${qdtTotalCarne} gramas de carne</p>
     </div>
-  `
-  }else {
-    resultado.innerHTML += `
+  `;
+    } else {
+      resultado.innerHTML = `
     <div class="result-block">
       <img src="./assets/steak2.svg"/>
-      <p>${qdtTotalCarne/1000} Kg de carne</p>
+      <p>${qdtTotalCarne / 1000} Kg de carne</p>
     </div>
-  `
-  }
+  `;
+    }
 
-
-  resultado.innerHTML += `
+    resultado.innerHTML += `
     <div class="result-block">
       <img src="./assets/beer.svg"/>
-      <p>${Math.ceil(qdtTotalCerveja/355)} latinhas de cerveja</p>
+      <p>${Math.ceil(qdtTotalCerveja / 355)} latinhas de cerveja</p>
     </div>
-  `
-  resultado.innerHTML += `
+  `;
+    resultado.innerHTML += `
     <div class="result-block">
       <img class="blackdrink" src="./assets/drinks.png"/>
-      <p>${Math.ceil(qdtTotalBebidas/2000)} garrafas de refrigerante ou soda</p>
+      <p>${Math.ceil(
+        qdtTotalBebidas / 2000
+      )} garrafas de refrigerante ou soda</p>
     </div>
     </br>
    
-  `
-}
+  `;
+  }
 }
